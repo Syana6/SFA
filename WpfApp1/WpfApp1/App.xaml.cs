721131aa91f2cacc233bc3085387f66e09073462
Syana6 <31329860+Syana6@users.noreply.github.com>
2018-04-11 17:03:45 +0800

    Update App.xaml.cs

--- WpfApp1/WpfApp1/App.xaml.cs
+++ WpfApp1/WpfApp1/App.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows;
 namespace WpfApp1
 {
     /// <summary>
-    /// App.xaml 的互動邏輯
+    /// App.xaml 的互動邏輯123web
     /// </summary>
     public partial class App : Application
     {
